3ed28983b97c736ffe52c53aa520014a8fc01bf2
dinos <dinostupar78@gmail.com>
2026-04-03 21:28:39 +0200

    initialize RoomMatcher application with Maven setup and basic structure

---
+++ src/main/java/hr/tvz/roommatcher/RoomMatcherApplication.java
@@ -0,0 +1,13 @@
+package hr.tvz.roommatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomMatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/tvz/roommatcher/RoomMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tvz.roommatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomMatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
