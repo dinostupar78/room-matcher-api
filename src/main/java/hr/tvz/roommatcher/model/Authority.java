212b0c1cd7f65437b7fef2af9b91422f02ba5ef9
dinos <dinostupar78@gmail.com>
2026-04-27 13:49:11 +0200

    feat: add Authority entity for user role management

---
+++ src/main/java/hr/tvz/roommatcher/model/Authority.java
@@ -0,0 +1,21 @@
+package hr.tvz.roommatcher.model;
+import hr.tvz.roommatcher.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "authority")
+public class Authority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private UserRole role;
+}
