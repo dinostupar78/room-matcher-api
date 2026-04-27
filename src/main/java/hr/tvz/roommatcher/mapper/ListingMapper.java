3419d8dc97cf0abd10467b52329c0f5942e01943
dinos <dinostupar78@gmail.com>
2026-04-27 13:52:48 +0200

    feat: update ListingMapper to use new DTOs and add update functionality

--- src/main/java/hr/tvz/roommatcher/mapper/ListingMapper.java
+++ src/main/java/hr/tvz/roommatcher/mapper/ListingMapper.java
@@ -1,16 +1,20 @@
 package hr.tvz.roommatcher.mapper;
-import hr.tvz.roommatcher.dto.ListingRequestDTO;
-import hr.tvz.roommatcher.dto.ListingResponseDTO;
+import hr.tvz.roommatcher.dto.listing.ListingRequest;
+import hr.tvz.roommatcher.dto.listing.ListingResponse;
+import hr.tvz.roommatcher.dto.listing.UpdateListingRequest;
 import hr.tvz.roommatcher.model.Listing;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface ListingMapper {
-    ListingResponseDTO toListingResponseDTO(Listing listing);
+    ListingResponse toListingResponse(Listing listing);
 
-    Listing toListing(ListingRequestDTO listingRequestDTO);
+    Listing toListing(ListingRequest listingRequest);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "appUser", ignore = true)
+    void updateListingFromRequest(UpdateListingRequest request, @MappingTarget Listing listing);
 
 
 }
