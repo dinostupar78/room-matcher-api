30955d5880de5697a03b9776cbd2511b6a82b6ea
dinos <dinostupar78@gmail.com>
2026-04-27 13:48:31 +0200

    feat: implement AppUserService and AppUserImpl for user registration functionality

---
+++ src/main/java/hr/tvz/roommatcher/service/AppUserService.java
@@ -0,0 +1,8 @@
+package hr.tvz.roommatcher.service;
+
+import hr.tvz.roommatcher.dto.auth.RegisterRequest;
+import hr.tvz.roommatcher.dto.user.AppUserResponse;
+
+public interface AppUserService {
+    AppUserResponse registerUser(RegisterRequest registerRequest);
+}
