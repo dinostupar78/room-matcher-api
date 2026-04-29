b5278954de667c098d3e12676629cfe38b6f0243
dinos <dinostupar78@gmail.com>
2026-04-29 20:18:58 +0200

    feat: reorder annotations in ListingImage class for improved readability

--- src/main/java/hr/tvz/roommatcher/model/ListingImage.java
+++ src/main/java/hr/tvz/roommatcher/model/ListingImage.java
@@ -1,15 +1,14 @@
 package hr.tvz.roommatcher.model;
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Data
 @Entity
 @Table(name = "listing_image")
 public class ListingImage {
